ce40d1c6a4fc6e7e084ab7b3d8ddbea7d05183b8
iwag <iwagame@gmail.com>
2014-05-29 11:18:05 +0900

    Add gcc49

---
+++ gcc49/build.sh
@@ -0,0 +1,11 @@
+# 1.
+brew tap homebrew/versions
+
+# 2. Edit some formulas
+# vi ~/linuxbrew/Library/Taps/versions/gmp4.rb
+
+# 3.
+# brew install gmp4 mpfr2 libmpc08 isl011 
+
+# 4.
+# brew install gcc49 --disable
