36274923b16680e6d1fc957c2b22a5588d2a2e60
iwag <iwagame@gmail.com>
2017-04-16 23:53:36 -0700

    update

--- 05_mysql/config_mysql.sh
+++ 05_mysql/config_mysql.sh
@@ -1,9 +1,10 @@
 # setting mysql
-DB=mydb
+DB=testdb
+USER=test
 # sudo service mysqld start
 mysql -uroot -e "create database $DB;"
 mysql -uroot -e "create user $USER;"
-mysql -uroot -e "grant all on *.* to $USER@localhost identified by 'dummy';"
+mysql -uroot -e "grant all on *.* to $USER@'%' identified by 'dummy';"
 mysql -uroot -e "create user ro;"
-mysql -uroot -e "grant all on *.* to ro@localhost identified by 'dummy';"
+mysql -uroot -e "grant all on *.* to ro@'%' identified by 'dummy';"
 
